c316342b59c489543ba4538f4e314dd8f1aa4b35
Manohar Nuthalapati <103932056+mn185177@users.noreply.github.com>
2022-12-01 22:36:04 +0530

    Update old-branches.sh

--- scripts/old-branches.sh
+++ scripts/old-branches.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 ECHO='echo '
-for branch in $(git branch -a | sed 's/^\s*//' | sed 's/^remotes\///' | grep -v 'main$\|develop$'); do
-  if ! ( [[ -f "$branch" ]] || [[ -d "$branch" ]] ) && [[ "$(git log $branch --since "1 month ago" | wc -l)" -eq 0 ]]; then
+for branch in $(git branch -r | sed 's/^\s*//' | sed 's/^remotes\///' | grep -v 'master$\|qa$'); do
+  if ! ( [[ -f "$branch" ]] || [[ -d "$branch" ]] ) && [[ "$(git log $branch --since "1 day ago" | wc -l)" -eq 0 ]]; then
     if [[ "$DRY_RUN" = "true" ]]; then
       ECHO=""
     fi
     local_branch_name=$(echo "$branch" | sed 's/remotes\/origin\///')
-    $ECHO git branch -d "${local_branch_name}"
+    branch_owner=$(git branch --contains=$local_branch_name --format=' %(authorname)' --sort=authorname)
+    $ECHO  "${local_branch_name}" -- "${branch_owner}"
     #$ECHO git push origin --delete "${local_branch_name}"
   fi
 done
